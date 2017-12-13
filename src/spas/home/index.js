db3e2ff6435d9377d442188a560182b672e8b800
Robert Greene <rgreene@zipwhip.com>
2017-12-13 10:57:06 -0800

    Setup webpack-dev-server

--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 console.log('hello worlds!')
 
-ReactDOM.render(<h1>Hello Worlds!</h1>, document.getElementById('react-spa'))
+ReactDOM.render(<h1>Hello Worldsss!</h1>, document.getElementById('react-spa'))
